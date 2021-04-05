a6c4e71e348e108cf26b2d92398277774bb703bf
devindi99 <devindikds@gmail.com>
2021-04-05 07:33:21 +0530

    shifting the pitch of audio signal

---
+++ pitch_shift.m
@@ -0,0 +1,42 @@
+function []= pitch_shift(AudioFile,n);
+[snd,fs] = audioread(AudioFile);
+%---------------------Representing the original audio signal---------------
+Xt=snd(:,1);
+info=audioinfo(AudioFile);
+t=0:1/fs:info.Duration-(1/fs);  
+Xf= abs(fft(Xt));
+f = linspace(-fs/2, fs/2+1, length(Xt));
+figure;
+subplot(2,2,1);
+plot(t,Xt);
+xlabel("Time(s)");
+ylabel("Amplitude");
+title("Original audio signal in time domain");
+subplot(2,2,2);
+plot(f,Xf);
+xlabel("Frequency(Hz)");
+ylabel("Magnitude");
+title("Original audio signal in frequency domain");
+
+%-----------------Shifting the pitch of the audio signal-------------------
+y=shiftPitch(snd,n);
+filename = 'pitched audio.wav';
+audiowrite(filename,y,fs);
+[Y,fs] = audioread(filename);
+sound(Y,fs)
+
+%-----------------Representing the pitched audio signal--------------------
+Y=Y(:,1);
+Xf= abs(fft(Y));
+f = linspace(-fs/2, fs/2+1, length(Y));
+subplot(2,2,3);
+plot(t,Y);
+xlabel("Time(s)");
+ylabel("Amplitude");
+title("Pitched audio signal in time domain");
+subplot(2,2,4);
+plot(f,Xf);
+xlabel("Frequency(Hz)");
+ylabel("Magnitude");
+title("Pitched audio signal in frequency domain");
+
