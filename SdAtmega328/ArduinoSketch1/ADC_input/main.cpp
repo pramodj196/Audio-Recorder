5782207ede2874a44b548dfb28773190c0d8e868
devindi99 <devindikds@gmail.com>
2021-06-16 21:10:11 +0530

    Adding an internal interrupt to write to file

--- SdAtmega328/ArduinoSketch1/ADC_input/main.cpp
+++ SdAtmega328/ArduinoSketch1/ADC_input/main.cpp
@@ -7,6 +7,7 @@
 #include <avr/io.h>
 #include <Arduino.h>
 #include <util/delay.h>
+#include <stdlib.h>
 //#include <stdio.h>
 #include "i2c_b.h"
 #include "LCD_i2c.h"
@@ -14,25 +15,11 @@
 #include <SD.h>
 //Beginning of Auto generated function prototypes by Atmel Studio
 //End of Auto generated function prototypes by Atmel Studio
-#define FASTADC 1
-#ifdef F_CPU
-#undef F_CPU
-#define F_CPU 8000000L
-#endif
-// defines for setting and clearing register bits
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
-String s = "";
-char s2[]={};
-int len;
-File myfile,myfile_R;
+#define F_CPU 8000000
+//int analogPin = PINC0
 const int chipselect = 10;
+File myfile,myfile_R;
 String line;
-String f_name =  "avr3.txt";
 uint8_t RECval, UPval, DOWNval, SELval, BACKval ;
 int Row=0;
 int Screen = 1;
@@ -46,27 +33,24 @@ void PlayClipII();
 void DeleteClipI();
 void DeleteClipII();
 void Back();
+unsigned int ADC_read(void);
 
 int main(void)
 {
+	 
 	 DDRD = 225;
-	 DDRD &=~((1<<PIND0)|(1<<PIND1)|(1<<PIND2)|(1<<PIND3)|(1<<PIND4)) ;
-	 /*Serial.begin(9600);
+	 DDRD &=~((1<<PIND5)|(1<<PIND6)|(1<<PIND2)|(1<<PIND3)|(1<<PIND4)) ;
+	 Serial.begin(9600);
 	 while (!Serial){
 		 ;
-	 }*/
-	 #if FASTADC
-	 // set prescale to 16
-	 sbi(ADCSRA,ADPS2) ;
-	 cbi(ADCSRA,ADPS1) ;
-	 cbi(ADCSRA,ADPS0) ;
-	 #endif
-	 if(!SD.begin(chipselect)){
-		 //Serial.println("Card failed or not present !");
-		 while(1);
 	 }
-	 
-	
+	Serial.println("Initializing the SD card...");
+	if(!SD.begin(chipselect)){
+		//Serial.println("Card failed or not present !");
+		
+		while(1);
+	}
+	Serial.println("Card initialized.");
 	i2c_init();
 	i2c_start();
 	i2c_write(0x70);
@@ -75,11 +59,22 @@ int main(void)
 	_delay_ms(50);
 	Menu();
 	/* Replace with your application code */
+	/*ADCSRA = 0x00; //disable adc
+	ADMUX  = 0x40;  //select adc input 0, ref:AVCC
+	ADCSRA = 0x82; //prescaler:4, single conversion mode
+	ADCSRA |= (1<<ADEN);*/
+	ADCSRA = ADCSRA | (1 << ADEN);
+	ADCSRA = ADCSRA | (1 << ADPS2);
+	ADMUX  = ADMUX   | (1 << ADLAR);
+	ADCSRA = ADCSRA  | (1 << ADIE);
+	sei();
+	//ADCSRA = ADCSRA | (1 << ADSC);
+	
 	while (1)
 	{
 		
-		RECval = PIND & (1<<PIND0); /*1*/
-		UPval = PIND & (1<<PIND1);/*2*/
+		RECval = PIND & (1<<PIND5); /*32*/
+		UPval = PIND & (1<<PIND6);/*64*/
 		DOWNval = PIND & (1<<PIND2);//4
 		SELval = PIND & (1<<PIND3);//8
 		BACKval = PIND & (1<<PIND4); //16
@@ -100,39 +95,37 @@ int main(void)
 		
 	}
 }
+ISR(ADC_vect){
+	//PORTB = ADCH ;
+	char ADCresult[4];
+	itoa(ADCH,ADCresult,10);
+	int k =  ADCH ;
+	myfile.write(ADCresult,4);
+	myfile.write('\n');
+	//ADCSRA = ADCSRA | (1 << ADSC);
+}
 void Recording(){
-	if (RECval ==1 ){
-		myfile = SD.open(f_name,FILE_WRITE);		
-		while ( RECval == 1 )
+	if (RECval ==32 ){
+		 myfile = SD.open("NewAVR05.txt",FILE_WRITE);
+		 lcd_cmd(0x01);	
+		 lcd_msg("RECORDING");	
+		while ( RECval == 32 )
 		{
-			lcd_cmd(0x01);
-			lcd_msg("Recording");
-			for (int i = 0; i <= 4; i++){
+			//lcd_cmd(0x01);
+			ADCSRA = ADCSRA | (1 << ADSC);
+			
+			/*for (int i = 0; i <= 4; i++){
 				lcd_msg(".");
 				_delay_ms(50);
-			}
-			RECval = PIND & (1<<PIND0);
-			lcd_cmd(0x01);
-			int y1=analogRead(A0);
-		
-			int mapped = map(y1, 0, 1023, 0, 255);
-			s.concat(mapped);
-
-			if (mapped<10) {
-				char s2[] = {s[0],' '};
-				myfile.write(s2,2);
-			}
-			else if (mapped<100) {
-				char s2[] = {s[0],s[1],' '};
-				myfile.write(s2,3);
-			}
-			else {
-				char s2[] = {s[0],s[1],s[2],' '};
-				myfile.write(s2,4);
-			}
-			s="";
+			}*/
+			RECval = PIND & (1<<PIND5);
+			//lcd_cmd(0x01);
+			//int x = ADC_read();
+			//myfile.println(x);
+			
 		}
 	myfile.close();
+	lcd_cmd(0x01);
 	lcd_msg("File Saved");
 	_delay_ms(1000);
 	Menu();
@@ -141,9 +134,38 @@ void Recording(){
 	}
 }
 		
+/*unsigned int ADC_read(void)
+{
+	char i;
+	unsigned int ADC_temp, ADCH_temp;
+	unsigned int ADC_var = 0;
+	ADCSRA |= (1<<ADSC);
+	while(!(ADCSRA & 0x10)); // wait for conversion done, ADIF flag active
+	ADCSRA|=(1<<ADIF);
+	ADC_temp = ADCL;
+	int sample = map(ADC_var,0,1023,0,255);
+	
+	/*for(i=0;i<8;i++)             // do the ADC conversion 8 times for better accuracy
+	{
+		ADCSRA |= (1<<ADSC);
+		while(!(ADCSRA & 0x10)); // wait for conversion done, ADIF flag active
+		ADCSRA|=(1<<ADIF);
+		
+		ADC_temp = ADCL;         // read out ADCL register
+		ADCH_temp = ADCH;        // read out ADCH register
+		ADC_temp +=(ADCH_temp << 8);
+		ADC_var += ADC_temp;      // accumulate result (8 samples) for later averaging
+	}
+
+	ADC_var = ADC_var >> 3;       // average the 8 samples
+	int sample = map(ADC_var,0,1023,0,255);
+	if(ADC_var > 255) {ADC_var = 255;}
+	
+	return sample;
+}*/
 
 void Navigate(){
-	if (UPval==2)
+	if (UPval==64)
 	{
 		lcd_cmd(0x80);
 		Row=1;
@@ -208,12 +230,12 @@ void PlayClipII(){
 		lcd_cmd(0x01);
 		lcd_cmd(0x80);
 		lcd_msg("Playing Audio 1");
-		myfile_R = SD.open(f_name,FILE_READ);
+		myfile_R = SD.open("test04.txt",FILE_READ);
 		if (myfile_R) {
 			while (myfile_R.available()) {
-				line=myfile_R.readStringUntil(' ');
+				line=myfile_R.readStringUntil('\n');
 				PORTD = line.toInt();
-				}
+			}
 			myfile_R.close();
 		}
 	}
